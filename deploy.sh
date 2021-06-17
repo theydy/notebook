ffb7e86007319e7091c26ad0c6a3599ab931f858
theydy <ydythebs@qq.com>
2021-06-17 16:44:59 +0800

    fix: 部署脚本

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/theydy/notebook.git master:gh-pages
+git push -f git@github.com:theydy/notebook.git master:gh-pages
 
 cd -
